f7ec34634ad3e1215f5f5667448e0a82342c0f80
David Filimon <david.filimon@amdaris.com>
2026-03-10 10:30:41 +0200

    leftovers from previous commit

--- LivelySheets.CatalogService.Domain/Entities/Messages/OutboxMessage.cs
+++ LivelySheets.CatalogService.Domain/Entities/Messages/OutboxMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LivelySheets.CatalogService.Domain.Entities.Messages;
+﻿namespace LivelySheets.CatalogService.Domain.Entities.Messages;
 
 public class OutboxMessage : Entity
 {
